d2d22d4c04911e9bf66f87ffb213b18bf5afeca4
cz <cz0930@hotmail.com>
2014-03-16 15:26:47 +0800

    rmcs

--- flask_learn/httpserver.py
+++ flask_learn/httpserver.py
@@ -1,42 +1,48 @@
-from flask import Flask,url_for,request,render_template
+from flask import Flask,url_for,request,render_template,make_response,abort,redirect,escape,session
 app = Flask(__name__)
 
-@app.route('/index/')
+@app.route('/send_cookie',methods=['GET', 'POST'])
+def send_cookie():
+    resp = make_response(render_template('login.html'))
+    resp.set_cookie('username','kingsley')
+    return resp
+
+@app.route('/recv_cookie',methods=['GET', 'POST'])
+def recv_cookie():
+    username = request.cookies.get('username')
+    return username
+
+#@app.route('/')
+#def index():
+#    return redirect(url_for('login'))
+
+#@app.route('/login')
+#def login():
+#    abort(401)
+#    this_is_never_executed()
+
+@app.route('/')
 def index():
-    return render_template('login.html')
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post %d' % post_id
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    return 'The about page'
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        if len(request.form["username"]) < 8 or len(request.form["password"]) < 8:
-            error = "username or password is invalid"
-        else:
-            error = "login success"
-    else:
-        return 'error not support get request'
-    return render_template('regut.html',error=error)
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''<form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+            </form>'''
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug = True)

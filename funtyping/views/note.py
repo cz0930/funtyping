e2f1b06c83b7af899333fb55b634741caf3e17a4
cz <cz0930@hotmail.com>
2014-03-18 08:14:51 +0800

    20140518

--- funtyping/views/note.py
+++ funtyping/views/note.py
@@ -4,6 +4,7 @@ from flask import render_template,request,flash,redirect,url_for,session,escape,
 from funtyping.models.user_models import *
 import datetime
 from funtyping.utils.util import get_user_id
+from random import randint
 
 @app.route('/latest')
 def latest():
@@ -34,7 +35,7 @@ def write():
             write_note(user_id,note_date,note_content)
         else:
             return redirect(url_for('login'))
-    return render_template('write.html')
+    return redirect(url_for('notes'))
 
 @app.route('/notes/<datenum>')
 @app.route('/notes/')
@@ -59,18 +60,48 @@ def notes(datenum=None):
             date_list = list(set(date_list))
     else:
         return redirect(url_for('login'))
-    return render_template('note_list.html', notes=notes , date_list=date_list)
+    for nn in notes:
+        nn.weekday = nn.time.strftime('%A')
+        nn.time = nn.time.strftime('%Y-%m-%d')
+    return render_template('note_list.html', notes=notes , date_list=date_list )
 
 @app.route('/get_newer_note', methods=['GET'])
 def get_newer_note():
     if get_user_id():
         note_id = int(request.args.get('note_id'))
         user_id = get_user_id()
-        note = Note.query.filter(Note.user_id == user_id, Note.id>note_id).order_by('id').first()
+        note = Note.query.filter(Note.user_id == user_id, Note.id<note_id).order_by('id').first()
         if not note:
-            note = Note.query_obj.get_note_by_id(note_id)
+            note = Note.query.filter_by(user_id=user_id).order_by('-id').first()
         note_id = note.id
         note_content = note.content            
-        note_weekday = note.time.weekday()
-        note_time = note.time.date()
+        note_weekday = note.time.strftime('%A')
+        note_time = note.time.strftime('%Y-%m-%d')
+    return jsonify(id = note_id,weekday = note_weekday,time = note_time,content = note_content)
+
+@app.route('/get_older_note', methods=['GET'])
+def get_older_note():
+    if get_user_id():
+        note_id = int(request.args.get('note_id'))
+        user_id = get_user_id()
+        note = Note.query.filter(Note.user_id == user_id, Note.id>note_id).order_by('id').first()
+        if not note:
+            note = Note.query.filter_by(user_id=user_id).order_by('id').first()
+        note_id = note.id
+        note_content = note.content
+        note_weekday = note.time.strftime('%A')
+        note_time = note.time.strftime('%Y-%m-%d')
+    return jsonify(id = note_id,weekday = note_weekday,time = note_time,content = note_content)
+
+@app.route('/get_random_note', methods=['GET'])
+def get_random_note():
+    if get_user_id():
+        note_id = int(request.args.get('note_id'))
+        user_id = get_user_id()
+        note_list = Note.query.filter_by(user_id=user_id).all()
+        note = note_list[randint(0, len(note_list)-1)]
+        note_id = note.id
+        note_content = note.content
+        note_weekday = note.time.strftime('%A')
+        note_time = note.time.strftime('%Y-%m-%d')
     return jsonify(id = note_id,weekday = note_weekday,time = note_time,content = note_content)
